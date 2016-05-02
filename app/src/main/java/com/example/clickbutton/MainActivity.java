01702a5997f218ba589afc6074b63aafed7b06c2
ergobot <stobryan@gmail.com>
2016-05-02 14:17:57 -0500

    Fixed layout and tested

--- app/src/main/java/com/example/clickbutton/MainActivity.java
+++ app/src/main/java/com/example/clickbutton/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.clickbutton;
 
 import android.content.Context;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
